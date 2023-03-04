1e2e291583c900c9091dfa490a6cf28a637f78bd
rdroe <d@vidroe.com>
2023-03-04 13:15:42 -0600

    export fix

--- src/util/exports.ts
+++ src/util/exports.ts
@@ -1,3 +1,7 @@
 export * from './cliParser'
 export * from './types'
 export * from './validation'
+export * from './index'
+export { default as strlog } from './strlog'
+export * from './predicates'
+export { default as awaitAll } from './awaitAll'
