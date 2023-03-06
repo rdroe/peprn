6cdac6cc27f1e246f92f397c8ee23f5b735a785d
rdroe <d@vidroe.com>
2023-03-06 17:30:12 +0000

    add metafile scaffolding to build js

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-
+const fs = require('node:fs')
 require('esbuild').build({
     external: ['node:repl'],
     entryPoints: ['./dist/testable/app.js'],
@@ -6,6 +6,19 @@ require('esbuild').build({
     outfile: './public/js/main.js',
     sourcemap: false,
     format: 'esm',
-    platform: 'neutral'
+    platform: 'neutral',
+    minify: true,
+    // metafile: true
+}).then((dat) => {
+    console.log('dat', dat)
+    if (dat.metafile) {
+        console.log('writing!!!', process.cwd())
+        fs.writeFile('dist/metafile.json', JSON.stringify(dat.metafile), (err) => {
+
+            console.log('wrote')
+            console.log('error', err?.message)
+        });
+    }
+
 }).catch(() => process.exit(1))
 
