4cd93ad1d8b7eb50dd3b3cd55aeca2a6bc979895
rdroe <d@vidroe.com>
2023-05-02 20:06:30 -0500

    umd

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import { isNode } from 'util/index'
+
 export { createApp as createServerApp } from './node'
-export { createApp as createBrowserApp } from './browser'
+import { createApp as createBrowserApp } from './browser'
+export { createBrowserApp }
 export * from './evaluator'
+
+if (!isNode()) {
+    (window as any).createBrowserApp = createBrowserApp
+}
