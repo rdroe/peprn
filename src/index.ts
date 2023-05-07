3de25661b982b7bc674c80e220039ff5dded4908
rdroe <d@vidroe.com>
2023-05-06 19:09:51 -0500

    browser only work

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export { createApp as createServerApp } from './node'
+// export { createApp as createServerApp } from './node'
+
 export { createApp as createBrowserApp } from './browser'
 export * from './evaluator'
