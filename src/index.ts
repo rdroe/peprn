fde92b7dcc47138f9ce997fd66ce1fb998963ed3
rdroe <d@vidroe.com>
2023-07-10 02:05:38 +0000

    export match

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { isNode } from 'util/index'
-
+import { isNode } from './util/index'
+import match from './match'
 export { createApp as createServerApp } from './node'
 import { createApp as createBrowserApp } from './browser'
 export { createBrowserApp }
+export { match }
 export * from './evaluator'
 
 if (!isNode()) {
